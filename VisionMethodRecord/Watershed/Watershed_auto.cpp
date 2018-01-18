caf3169bf23c9abe92b3057ec9bd16aab87cdf20
sykzhong <2426085931@qq.com>
2018-01-18 11:37:19 +0800

    增加pictures文件夹方便图片呢管理

--- VisionMethodRecord/Watershed/Watershed_auto.cpp
+++ VisionMethodRecord/Watershed/Watershed_auto.cpp
@@ -42,7 +42,7 @@ Watershed_auto::~Watershed_auto()
 
 void Watershed_auto::getImage()
 {
-	srcimage = cv::imread("Pictures//watershed_test2.jpg", CV_LOAD_IMAGE_COLOR);
+	srcimage = cv::imread("Pictures//watershed//houghtest.jpg", CV_LOAD_IMAGE_COLOR);
 	//imshow("srcimage", srcimage);
 	//cv::waitKey(0);
 }
@@ -54,28 +54,33 @@ void Watershed_auto::threshImage()
 	threshed_image = cv::Mat(srcimage.size(), CV_8UC1);
 	//cv::threshold(srcimage_gray, threshed_image, 0, 255, CV_THRESH_BINARY_INV | CV_THRESH_OTSU);
 	cv::threshold(srcimage_gray, threshed_image, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
+	cv::namedWindow("threshed_image", CV_WINDOW_NORMAL);
 	imshow("threshed_image", threshed_image);
 	cv::waitKey(0);
 }
 
 void Watershed_auto::getForeBackImage()
 {
-	int morph_elem = 2;		//Element:\n 0: Rect - 1: Cross - 2: Ellipse
-	int morph_size = 3;
+	int morph_elem = 2;				//Element:\n 0: Rect - 1: Cross - 2: Ellipse
+	int morph_size = 2;
 	int morph_operation = 0;		//0: Opening - 1: Closing \n 2: Gradient - 3: Top Hat \n 4: Black Hat
 	cv::Mat element = cv::getStructuringElement(morph_elem, cv::Size(2 * morph_size + 1, 2 * morph_size + 1), cv::Point(morph_size, morph_size));
 	cv::Mat opening;
-	cv::morphologyEx(threshed_image, opening, morph_operation, element, cv::Point(-1, -1), 2);
+	//cv::morphologyEx(threshed_image, opening, morph_operation, element, cv::Point(-1, -1), 2);
+	cv::erode(threshed_image, opening, element);
+	cv::namedWindow("opening", CV_WINDOW_NORMAL);
 	imshow("opening", opening);
 	cv::waitKey(0);
 
 	cv::dilate(threshed_image, back_image, element, cv::Point(-1, -1), 3);
+	cv::namedWindow("back_image", CV_WINDOW_NORMAL);
 	imshow("back_image", back_image);
 	cv::waitKey(0);
 
 	cv::Mat distance;
 	cv::distanceTransform(opening, distance, CV_DIST_L2, 5);
 	//distance.convertTo(distance, CV_8UC1);
+	//cv::namedWindow("distance", CV_WINDOW_NORMAL);
 	//imshow("distance", distance);		//sykfix: 做归一化？
 	//归一化
 
@@ -85,12 +90,14 @@ void Watershed_auto::getForeBackImage()
 	cv::minMaxLoc(distance, NULL, &maxdistance);
 	cout << maxdistance;
 	cv::threshold(distance, fore_image, 0.03*maxdistance, 255, 0);
+	cv::namedWindow("fore_image", CV_WINDOW_NORMAL);
 	imshow("fore_image", fore_image);
 	cv::waitKey(0);
 
 	fore_image.convertTo(fore_image, CV_8UC1);
 	back_image.convertTo(back_image, CV_8UC1);
 	cv::subtract(back_image, fore_image, unsure_image);
+	cv::namedWindow("unsure_image", CV_WINDOW_NORMAL);
 	imshow("unsure_image", unsure_image);
 	cv::waitKey(0);
 }
@@ -110,7 +117,7 @@ void Watershed_auto::getMarkers()
 	int Channels = unsure_image.channels();
 	int Rows = unsure_image.rows;
 	int Cols = unsure_image.cols;
-	int *p;			//指代markers的行指针
+	int *p;				//指代markers的行指针
 	const int *q;		//指代unsure_image的行指针
 					
 	int index = 0;		//用于记录三通道的末下标
@@ -157,7 +164,7 @@ void Watershed_auto::showResult()
 		for (int j = 0; j < markers.cols; j++)
 		{	// 根据经过watershed处理过的maskWaterShed来绘制每个区域的颜色
 			int index = markers.at<int>(i, j);  // 这里的maskWaterShed是经过watershed处理的
-			if (index == -1)  // 区域间的值被置为-1（边界）
+			if (index == -1)					// 区域间的值被置为-1（边界）
 				resImage.at<cv::Vec3b>(i, j) = cv::Vec3b(255, 255, 255);
 			else if (index <= 0 || index > maxmark)  // 没有标记清楚的区域被置为0
 				resImage.at<cv::Vec3b>(i, j) = cv::Vec3b(0, 0, 0);
@@ -166,7 +173,9 @@ void Watershed_auto::showResult()
 		}
 	}
 	//addWeighted(resImage, 0.3, srcimage, 0.7, 0, resImage);
+	cv::namedWindow("result", CV_WINDOW_NORMAL);
 	imshow("result", resImage);
+	cv::namedWindow("markers", CV_WINDOW_NORMAL);
 	imshow("markers", markers);
 	cv::waitKey(0);
 }
